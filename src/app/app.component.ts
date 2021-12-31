e9cdf013186ec7ba97a76265b88502b2490a664d
electroluxv2 <electroluxv2@gmail.com>
2021-12-31 06:09:08 +0100

    Cards test 4 trays

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
 import { CardService } from './services/card.service';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  cards: string[] = [];
+  bottomCards: string[] = [];
+  topCards: string[] = [];
+  leftCards: string[] = [];
+  rightCards: string[] = [];
 
   constructor(public cardService: CardService) {
-    cardService.getCards().then(c => this.cards = c);
+    cardService.getCards().then(c => this.bottomCards = c);
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
+    }
   }
 }
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,17 +1,27 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { fromEvent, takeUntil, timer } from 'rxjs';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent implements OnInit {
-
+export class CardComponent implements AfterViewInit {
+  public enlarged = false;
   @Input("cardURL") cardURL = '';
+  @ViewChild('container') container?: ElementRef;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngAfterViewInit(): void {
+    const mousedown = fromEvent(this.container?.nativeElement, 'mousedown');
+    const mouseup = fromEvent(this.container?.nativeElement, 'mouseup');
 
+    mousedown.subscribe(e => {
+      const clickTimer = timer(200);
+      mouseup.pipe(takeUntil(clickTimer)).subscribe(e => {
+        this.enlarged = !this.enlarged;
+      });
+    });
+  }
 }
